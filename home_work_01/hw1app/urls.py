78bd00838f5b12cc0c31e4a31e0387920d724550
nikshansherov <nikshansherov@yandex.ru>
2023-08-22 16:59:28 +0300

    modified urls.py, views.py

--- home_work_01/hw1app/urls.py
+++ home_work_01/hw1app/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('games/', views.games, name='games'),
+    path('cube/', views.cube, name='cube'),
+    path('heads_or_tails/', views.heads_or_tails, name='heads_or_tails'),
+    path('random_number/', views.random_number, name='random_number'),
 ]
--- home_work_01/hw1app/views.py
+++ home_work_01/hw1app/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import logging
+from random import randint, choice
 
 logger = logging.getLogger(__name__)
 
-TEXT1 = f'<h1>Я Николай, и это моя страничка</h><br>' \
-        f'<a href="/about">О себе</a>'
+TEXT1 = f'<h1>Я Николай, и это моя страничка<br>Здесь вроде бы как ' \
+        f'расположен многострочный текст :)</h><br>' \
+        f'<a href="/about">О себе</a><br>' \
+        f'<a href="/games">Поиграй</a>'
 TEXT2 = f'<h1>Я такой весь умный, красивый, а еще и скромный... ХА-ХА-ХА!!!</h><br>' \
+        f'<a href="/">Главная</a><br>' \
+        f'<a href="/games">Поиграй</a>'
+TEXT3 = f'<h1>Выбирете игру</h><br>' \
+        f'<a href="/cube">Бросить кубик<br></a>' \
+        f'<a href="/heads_or_tails">Орел или решка<br></a>' \
+        f'<a href="/random_number">Случайное число<br></a>' \
         f'<a href="/">Главная</a>'
 
 
@@ -16,3 +25,29 @@ def index(request):
 
 def about(request):
     return HttpResponse(TEXT2)
+
+
+def games(request):
+    return HttpResponse(TEXT3)
+
+
+def cube(request):
+    number = randint(1, 6)
+    logger.info(f'Result - {number}')
+    return HttpResponse(f'<h1>Ваш бросок кубика - {number}</h><br>'
+                        f'<a href="/games">Вернуться</a><br>')
+
+
+def heads_or_tails(request):
+    cast = choice(['Орел', 'Решка'])
+    logger.info(f'Result - {cast}')
+    return HttpResponse(f'<h1>Вы бросили - {cast}</h><br>'
+                        f'<a href="/games">Вернуться</a><br>')
+
+
+def random_number(request):
+    number = randint(0, 100)
+    logger.info(f'Result - {number}')
+    return HttpResponse(f'<h1>Ваше число - {number}</h><br>'
+                        f'<a href="/games">Вернуться</a><br>')
+
